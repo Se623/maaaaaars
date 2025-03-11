2520559a7032ce77abaa6005a02ec15d3717c1dd
Se623 <75635764+Se623@users.noreply.github.com>
2025-03-11 17:48:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/index/<word>')
 @app.route('/<word>')
 def mars(word):
-    return render_template('index.html', title=word)
+    return render_template('base.html', title=word)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
